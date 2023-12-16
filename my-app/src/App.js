1cede130462ede4a4babd72e898b7cdf2e37711d
Daniel <daniel.nogueira.dacosta@gmail.com>
2023-12-16 17:44:57 -0300

    start

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SISCAN</h1>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+function HelloWorld(){
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <p>This is my first component</p>
+        </div>
+    )
+}
+
+export default HelloWorld
